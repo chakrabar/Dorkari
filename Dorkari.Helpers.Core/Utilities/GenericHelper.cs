0adfd54c5792fe49fceeb8587f673573053d0946
chakrabar <chakrabar@live.com>
2015-12-10 21:27:05 +0530

    Adding generic helper
    Adding generic helper

---
+++ Dorkari.Helpers.Core/Utilities/GenericHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dorkari.Helpers.Core.Utilities
+{
+    public class GenericHelper
+    {
+        public static T SelectByPredicate<T>(T value, Func<T, bool> predicate, T alternativeValue)
+        {
+            return predicate(value) ? value : alternativeValue;
+        }
+
+        public static T UseOtherIfNull<T>(T value, T other) where T : class
+        {
+            return SelectByPredicate(value, x => x != null, other);
+        }
+    }
+}
