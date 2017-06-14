ce316e2460c768bd4908c724c08b09f3dede8683
Arghya C <chakrabar@live.com>
2017-06-15 03:35:29 +0530

    test samples

---
+++ Dorkari.Samples.Cmd/Tests/BoolCondition.cs
@@ -0,0 +1,42 @@
+﻿namespace Dorkari.Samples.Cmd.Tests
+{
+    class BoolCondition
+    {
+        int TestBool(bool isGood) //these all are comiled to !isGood
+        {
+            int blah = 0;
+            if (isGood != true)
+            {
+                blah++;
+            }
+            if (!(isGood == true))
+            {
+                blah += 2;
+            }
+            if (!isGood)
+            {
+                blah -= 3;
+            }
+            return blah;
+        }
+
+        int TestString(string name) //these all are compiled differently (as in code)
+        {
+            int blah = 0;
+            string defaultName = "whatever";
+            if (name != defaultName)
+            {
+                blah++;
+            }
+            if (!(name == defaultName))
+            {
+                blah += 2;
+            }
+            if (!name.Equals(defaultName))
+            {
+                blah -= 3;
+            }
+            return blah;
+        }
+    }
+}
