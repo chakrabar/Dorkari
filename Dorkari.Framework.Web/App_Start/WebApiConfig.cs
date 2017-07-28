3f74a29e6ad212920834a58a7f773bf0df9d3d42
Arghya C <chakrabar@live.com>
2017-07-29 01:26:21 +0530

    The web framework

---
+++ Dorkari.Framework.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace Dorkari.Framework.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services            
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Additional optional configuration
+            //var routes = config.Routes;
+            ////routes.MapHttpRoute("DefaultApiWithId", "Api/{controller}/{id}", new { id = RouteParameter.Optional }, new { id = @"\d+" }); new string[] { "GET" }
+            //routes.MapHttpRoute("DefaultApiWithAction", "Api/{controller}/{action}");
+            //routes.MapHttpRoute("DefaultApiGet", "Api/{controller}", new { action = "Get" }, new { httpMethod = new HttpMethodConstraint(System.Net.Http.HttpMethod.Get) });
+            //routes.MapHttpRoute("DefaultApiPost", "Api/{controller}", new { action = "Post" }, new { httpMethod = new HttpMethodConstraint(System.Net.Http.HttpMethod.Post) });
+            //routes.MapHttpRoute("DefaultApiPut", "Api/{controller}", new { action = "Put" }, new { httpMethod = new HttpMethodConstraint(System.Net.Http.HttpMethod.Put) });
+
+            //config.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("format", "json", "application/json"));
+            //config.Formatters.XmlFormatter.MediaTypeMappings.Add(new QueryStringMapping("format", "xml", "application/xml"));
+            //config.Formatters.XmlFormatter.UseXmlSerializer = true;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new DefaultContractResolver();
+
+            //var json = config.Formatters.JsonFormatter;
+            //json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            ////config.Formatters.Remove(config.Formatters.XmlFormatter); //It STOPS support for XML media type
+            //json.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+    }
+}
