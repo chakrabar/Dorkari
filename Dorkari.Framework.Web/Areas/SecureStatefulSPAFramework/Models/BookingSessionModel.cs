64c3c8b2760dc64a334e20d0486f38284612af11
Arghya C <chakrabar@live.com>
2017-08-08 16:44:10 +0530

    S3PA missed model

---
+++ Dorkari.Framework.Web/Areas/SecureStatefulSPAFramework/Models/BookingSessionModel.cs
@@ -0,0 +1,16 @@
+﻿using Dorkari.Framework.Web.Areas.SecureStatefulSPAFramework.Models.Enums;
+using System;
+
+namespace Dorkari.Framework.Web.Areas.SecureStatefulSPAFramework.Models
+{
+    public class BookingSessionModel
+    {
+        public BookTicketStep NextStep { get; set; }
+        public string UserEmail { get; set; }
+        public string MovieName { get; set; }
+        public DateTime MovieTime { get; set; }
+        public int TicketCount { get; set; }
+        public decimal TicketCost { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
