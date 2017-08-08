2d7bb42f3782e238c38c6f44b1346e05880fb0f6
Arghya C <chakrabar@live.com>
2017-08-08 15:59:31 +0530

    Angular shared directives

---
+++ Dorkari.Framework.Web/Scripts/App/Shared/Directives/HtmlBinderModule.js
@@ -0,0 +1,13 @@
+﻿angular.module('HtmlBinderModule', [])
+    .directive('bindHtmlString', ['$compile', '$parse', function ($compile, $parse) { //use: <div bind-html-simple="viewData" ng-init="funcToPopulateViewData()"></div>
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                //element.addClass('ng-binding').data('$binding', attr.bindNgHtml);
+                scope.$watch(attr.bindHtmlSimple, function () {
+                    element.html($parse(attr.bindHtmlSimple)(scope));
+                    $compile(element.contents())(scope);
+                }, true);
+            }
+        };
+    } ]);
---
+++ Dorkari.Framework.Web/Scripts/App/Shared/Directives/SimpleAjaxLoaderModule.js
@@ -0,0 +1,25 @@
+﻿angular.module('SimpleAjaxLoaderModule', [])
+    .directive('simpleAjaxLoader', ['$http', '$rootScope', function ($http, $rootScope) { //use: <div simple-ajax-loader>...</div>
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                scope.isLoading = function () {
+                    if (scope.showLoader.value)
+                        return scope.showLoader.value;
+                    else
+                        scope.isLoaderVisible.value = $http.pendingRequests.length > 0;
+                    return scope.isLoaderVisible.value;
+                };
+                var focused = null;
+                scope.$watch(scope.isLoading, function (v) {
+                    if (v) {
+                        element.show(); //shows the directive element (ideally a full-screen overaly) until all ajax calls complete
+                    } else {
+                        element.hide();
+                        //$rootScope.$broadcast('ajaxLoadComplete');
+                    }
+                });
+            }
+        };
+
+    }]);
