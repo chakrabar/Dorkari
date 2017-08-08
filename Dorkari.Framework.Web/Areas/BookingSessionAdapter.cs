63b8b8a628964931a7ea6801cd42fbd411bcadf8
Arghya C <chakrabar@live.com>
2017-08-08 16:43:10 +0530

    S3PA updates

---
+++ Dorkari.Framework.Web/Areas/BookingSessionAdapter.cs
@@ -0,0 +1,50 @@
+﻿using Dorkari.Framework.Web.Areas.SecureStatefulSPAFramework.Models;
+using Dorkari.Framework.Web.Areas.SecureStatefulSPAFramework.Models.Enums;
+using Dorkari.Framework.Web.Helpers;
+using Dorkari.Framework.Web.Helpers.Contracts;
+using System;
+
+namespace Dorkari.Framework.Web.Areas
+{
+    public class BookingSessionAdapter
+    {
+        const string _BOOKING_SESSION_KEY = "UserBookingData";
+
+        readonly IStateHelper _sessionHelper;
+
+        public BookingSessionAdapter()
+        {
+            _sessionHelper = new WebSessionHelper();
+        }
+
+        internal Tuple<string, BookingViewModel> GetCurrentViewDetails()
+        {
+            var model = GetSessionData();
+            return new Tuple<string, BookingViewModel>(model.NextStep.ToString(), BookingViewModel.GetViewModel(model));
+        }
+
+        internal Tuple<string, BookingViewModel> UpdateSession(string email)
+        {
+            var sessionModel = GetSessionData();
+            if (sessionModel.NextStep == BookTicketStep.Welcome)
+                sessionModel = new BookingSessionModel();
+            if (!string.IsNullOrEmpty(email))
+            {
+                sessionModel.UserEmail = email; //TODO: write actual logic
+            }
+            SetSessionData(sessionModel);
+            return new Tuple<string, BookingViewModel>(sessionModel.NextStep.ToString(), BookingViewModel.GetViewModel(sessionModel));
+        }
+
+        private void SetSessionData(BookingSessionModel data)
+        {
+            _sessionHelper.AddData(_BOOKING_SESSION_KEY, data);
+        }
+
+        private BookingSessionModel GetSessionData()
+        {
+            var data = _sessionHelper.GetData<BookingSessionModel>(_BOOKING_SESSION_KEY);
+            return data ?? new BookingSessionModel();
+        }
+    }
+}
--- Dorkari.Framework.Web/Areas/SecureStatefulSPAFramework/Controllers/SSSPAController.cs
+++ Dorkari.Framework.Web/Areas/SecureStatefulSPAFramework/Controllers/SSSPAController.cs
@@ -1,13 +1,44 @@
-﻿using System.Web.Mvc;
+﻿using Dorkari.Framework.Web.Areas.SecureStatefulSPAFramework.Models;
+using System.Web.Mvc;
+using Dorkari.Framework.Web.Helpers;
+using Dorkari.Framework.Web.Areas.SecureStatefulSPAFramework.Models.Enums;
 
 namespace Dorkari.Framework.Web.Areas.SecureStatefulSPAFramework.Controllers
 {
     public class SSSPAController : Controller
     {
-        // GET: SecureStatefulSPAFramework/SSSPA
+        readonly BookingSessionAdapter _bookingAdaper;
+
+        public SSSPAController()
+        {
+            _bookingAdaper = new BookingSessionAdapter();
+        }
+
+        // GET: sp3a/SSSPA
         public ActionResult Index()
         {
             return Content("Welcome to the Secure Stateful SPA aka s3pa");
+        }
+
+        public ActionResult IndexToBeFixed(string email = "")
+        {
+            var initialViewAndModel = _bookingAdaper.UpdateSession(email);
+            return View("BookingPageContainer",
+                new S3paJsonModelBase
+                {
+                    NextPageHtml = this.RenderPartialViewAsString(initialViewAndModel.Item1, initialViewAndModel.Item2),
+                    NextStepString = initialViewAndModel.Item1
+                }
+            );
+        }
+
+        private void UpdateViewData(S3paJsonModelBase jsonData) //to be called from ajax requests
+        {
+            if (jsonData != null && jsonData.ResponseStatus.Equals(BookingReponseStatus.Success))
+            {
+                var currentViewAndModel = _bookingAdaper.GetCurrentViewDetails();
+                jsonData.NextPageHtml = this.RenderPartialViewAsString(currentViewAndModel.Item1, currentViewAndModel.Item2);
+            }
         }
     }
 }
