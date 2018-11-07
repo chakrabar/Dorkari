28e78506beda0d05a0d3cb54ac0ce35acd97a720
Arghya C <chakrabar@live.com>
2018-11-07 09:04:16 +0530

    More tests

---
+++ Dorkari.Samples.Cmd/Tests/EmptyTypes.cs
@@ -0,0 +1,20 @@
+﻿namespace Dorkari.Samples.Cmd.Tests
+{
+    interface IEmptyInterface //builds FINE - interface can be empty
+    { }
+
+    class EmptyCLass //builds FINE - class can be empty
+    { }
+
+    static class EmptyStaticClass //builds FINE - static class can also be empty
+    { }
+
+    abstract class EmptyAbstractClass //builds FINE - abstract class can also be empty
+    { }
+
+    sealed class EmptySealedClass //builds FINE - sealed class can also be empty
+    { }
+
+    struct EmptyStruct //builds FINE - struct can be empty
+    { }
+}
--- Dorkari.Samples.Cmd/Tests/StaticAndSingleton.cs
+++ Dorkari.Samples.Cmd/Tests/StaticAndSingleton.cs
@@ -5,17 +5,31 @@ namespace Dorkari.Samples.Cmd.Tests
     interface ISomeContract
     {
         void DoSomething();
-    }
+    }    
 
     class SomeBaseClass
     {
         public int MyProperty { get; set; }
     }
 
-    //static classes cannot derive from class or implement interface
-    static class StaticAndSingleton //: SomeBaseClass //,ISomeContract
+    static class StaticBaseClass
+    {
+        public static string MyString { get; set; }
+    }
+
+    //STATIC classes CANNOT derive from (static or instance) class or implement interface
+    static class StaticAndSingleton //: SomeBaseClass //,ISomeContract //,StaticBaseClass
     {
         public static void DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //instance classes also CANNOT derive from STATIC
+    class AnotherClass : ISomeContract //: StaticBaseClass
+    {
+        public void DoSomething() //interface implementation method CANNOT be static
         {
             throw new NotImplementedException();
         }
